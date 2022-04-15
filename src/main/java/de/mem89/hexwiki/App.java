1bb679da2c337a4990642082929b61b2a5907320
mem89de <61516914+mem89de@users.noreply.github.com>
2022-04-15 22:44:56 +0200

    Minimal log4j2 configuration

--- src/main/java/de/mem89/hexwiki/App.java
+++ src/main/java/de/mem89/hexwiki/App.java
@@ -1,13 +1,16 @@
 package de.mem89.hexwiki;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+public class App {
+
+    public static void main(String[] args) {
+        Logger LOGGER = LogManager.getLogger(App.class);
+
+        LOGGER.info("This is an 'info' message");
+        LOGGER.warn("This is a 'warn' message");
+        LOGGER.error("This is an 'error' message");
+        LOGGER.debug("This is a 'debug' message");
+        LOGGER.trace("This is a 'trace' message");
     }
 }
